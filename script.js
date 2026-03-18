d7f482160c5a3343c65c9c129599cb849560b141
joaosouza73 <joaovictorpereiradesouza73@gmail.com>
2026-03-17 21:44:45 -0300

    adicionada a checkbox e o botão de finalizar tasks

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const input = document.getElementById("taskInput");
 const button = document.getElementById("addTaskBtn");
 const list = document.getElementById("taskList");
+const completedBtn = document.getElementById("completedTasksBtn");
 
 input.addEventListener("keypress", function(e){
-
     if(e.key === "Enter"){
         button.click();
     }
@@ -12,16 +12,17 @@ input.addEventListener("keypress", function(e){
 button.addEventListener("click", function () {
 
     const taskText = input.value.trim();
-
     if(taskText === "")return;
 
     const li = document.createElement("li");
 
-    li.textContent = taskText;
+    //checkbox
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
-    li.addEventListener("click", function (){
-        li.classList.toggle("completed");
-    });
+    // texto da tarefa
+    const span = document.createElement("span");
+    span.textContent = taskText;
 
     //botao de remover tarefas
     const deleteBtn = document.createElement("button");
@@ -32,10 +33,26 @@ button.addEventListener("click", function () {
         li.remove();
     });
 
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
     li.appendChild(deleteBtn);
     
     list.appendChild(li);
 
     input.value = "";
 
+});
+
+//concluir tarefas
+completedBtn.addEventListener("click", function(){
+
+    const task = document.querySelectorAll("#taskList li");
+    task.forEach(function(task){
+        const checkbox = task.querySelector("input");
+
+        if(checkbox.checked){
+            task.remove();
+        }
+    });
 });
